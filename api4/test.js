4e5ed6dc13b479ff103607dcd25a2a195602137b
amolkhune1 <akhune1@gmail.com>
2023-08-30 21:32:15 +0530

    test commit

--- api4/test.js
+++ api4/test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fn = async () => {
   let fileContent = await fs.readFileSync('./test.txt');
   console.log(fileContent.toString());
-  let newContent = fileContent.toString().replace(/Banana/g, 'apple');
+  let newContent = fileContent.toString().replace(/apple/g, 'Banana');
   fs.writeFileSync('./test.txt', newContent);
 };
 fn();
